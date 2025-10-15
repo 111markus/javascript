ddeec2c46c9e042e36c7dbc39725f3cb945c9d30
Markus <kyo@Markuss-MacBook-Air.local>
2025-10-15 15:10:47 +0300

    tunnis tehtud

---
+++ index.js
@@ -0,0 +1,39 @@
+//console.log("Hello");
+//console.log("I like pizza!");
+
+//window.alert("This is an alert");
+//window.alert("I like pizza!");
+
+//document.getElementById("myH1").textContent = "Hello";
+//document.getElementById("myP").textContent = "I like pizza!";
+
+// This is a comment
+
+/* 
+    This
+    Is
+    A
+    Comment
+*/
+
+/* Variable = container that stores value
+
+1. declaration let x;
+2. assignment x = 100;
+*/
+
+let x;
+x = 100;
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(age);
+console.log(price);
+console.log(gpa);
+
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(`Your GPA is " + ${gpa}`);
